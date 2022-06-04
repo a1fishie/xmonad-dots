9e8bf52fb459ff2bf3d7f17db6bae7a23ebc134a
a1fishie <104651507+a1fishie@users.noreply.github.com>
2022-06-04 16:50:03 +0000

    cried about haskell syntax

--- README.md
+++ README.md
@@ -1,2 +1,137 @@
 # xmonad-dots
-its in the name.
+
+todo: screenshots, rewriting half the star wars saga into the config (three quarters!!!)
+
+### include:
+
+- 5 hour, 3 days a week therapy sessions
+- three days worth of tears
+- bloat
+- random colors that came out of my vomit
+- a semi-functioning xmobar (doesnt work)
+- c l i c k y workspaces
+- a beautiful wallpaper
+- wait i forgot to upload the wallpaper
+- pop out terminal!! (scratchpads ftw, NSP = fake desktop)
+
+# install guide
+
+please ensure two semi-functioning limbs of your choosing are availble at present.
+
+1. Run `git clone https://github.com/a1fishie/xmonad-dots` into a directory you wish to perish.
+2. `cd xmonad-dots`
+3. simply copy the files from `.config` into your `.config` folder.
+4. forget everything, going on a tangent in order to ~~fix~~ implement unneccesary things.
+5. copy the `.xmonad` folder into your home directory
+6. remember to `sudo make install` the dmenu folder
+7. quit your current session by brutally snapping your cpu into millions of tiny pieces.
+8. configure your .xinitrc (basic ass hoe) or login manager (supremacy) to boot into xmonad
+9. voila! you have now obtained the most basic dots you could image, aren't you proud of yourself!
+
+# keybinds
+i won't lie to you, i dont know half of these myself
+
+<details><summary>domain e x p a n s i o n</summary>
+
+default meta = 'windows' key, if you prefer alt i will hunt you. (to tell you to change some other keybinds)
+you can also click the workspaces to move (except NSP for the reason above)
+
+| Keybind                     | Function                                                           |
+| :-------------------------- | ------------------------------------------------------------------ |
+| Ctrl+Meta+Alt+Q             | Quit.                                                              |
+| Meta+Alt+Q                  | Quit XMonad                                                        |
+| Meta+Alt+R                  | Recompile XMonad                                                   |
+| Meta+Shift+R                | Restart XMonad                                                     |
+| Meta+Enter                  | Open terminal in a scratchpad: toggleable window  (kitty)          |
+| Meta+S                      | Open Launcher (dmenu - yet to config properly)                     |
+| Meta+C                      | Open text editor of choice (default neovide)                       |
+| Meta+Shift+C                | Open your browser of choice (change variable near top)             |
+| Meta+E                      | Open file manager of choice (dolphin by default, questionable ik)  |
+| Ctrl+Alt+J/K/H/L            | Dec/Inrease WINDOW spacing  (J/K) Dec/Increase SCREEN spacing (H/L)|
+| PrintScrn                   | maim Screenshot (saved to clipboard)                               |
+| Meta+N                      | c o l o r p i c k e r!!! (hex)                                     |
+| Ctrl+Shift+Q                | Kills currently focused window                                     |
+| Meta+B                      | Refreshes current desktop??                                        |
+| Meta+Tab                    | Changes Layout (default threeCol and tall, add more in xmonad.hs)  |
+| Meta+J/K                    | Change window focus (vim-esque)                                    |
+| Meta+M                      | Focus master window                                                |
+| Meta+Backspace              | Promotes window to become master                                   |
+| Meta+Shift+J/K              | Move window left/right                                             |
+| Meta+Alt+H/L                | Shrink/grow focused window                                         |
+| Meta+T                      | Push window into tiling ("sink")                                   |
+| Mod+[1-9]                   | Change workspace                                                   |
+| Mod+Shift+[1-9]             | Move focused window to workspace                                   |
+| Mod+Shift+Up/Down           | Increase master window number (becomes apparent in tall layout)    |
+| Mod+MLeftClick              | Move window, places into floating                                   |
+| Mod+MRightClick             | Resize window in floating                                          |
+
+</details>
+
+# Dependencies
+<details><summary>wonder what could go here</summary>
+<pre> xmonad xmobar lxsession xorg-xrandr xorg-xprop python-pywal picom maim xcolor nerd-fonts-fira-code awesome-terminal-fonts nerd-fonts-arimo </pre>
+</details>
+
+
+# Khajit has fixes if you have coin, alternatively: troubleshooting
+
+### why is there blank icons what
+Simple, you or I forgot a font, which one I do not know. - will update soon enough
+
+### why caps no work????
+1. Open `.xmonad/change_layout.py`
+2. Change variable to `False`
+3. Recompile + Restart Xmonad (crashing Xorg also works)
+
+<details><summary>It says it can't compile cuz somethingsomething spotify</summary>
+
+You are really living in the 16th century if you are without spotify.
+anyway,
+1. Open xmonad.hs using your favorite text editor (neovim!!)
+2. Go to line 259 (259gg in neovim/vim)
+3. Remove/append `spawnOnce "spotify"` with either a dd (remove line) or by replacing `spotify` with your media player
+4. recompile
+
+If xmobar also cries, change `"spotify"` in `xmobarrc`, line 25 and recompile
+</details>
+
+<details><summary> why app look bad wah wah wah</summary>
+
+Basically, theres like 25 different theming platforms that you *can* use.
+I prefer LXAppearance and Qt5ct, with Kvantum of course.
+
+### Kvantum? You mean Quantum??? I thought that was physics???
+You'd be right except wrong quantum.
+
+1. Install Kvantum (community repo on Arch)
+2. Download QT5 theme from either KDE store or the AUR.
+3. Have an absolute breakdown because you can't find a good one
+4. Realize you dont need to do anything if you got it from the AUR
+5. Unzip the theme into a useless directory (i prefer /boot/efi)
+6. Launch Kvantum and under 'Select Kvantum Folder', navigate to where you extracted the theme
+7. Skip steps 5 onwards if you installed from the AUR
+8. Under `Change/Delete Theme` select your theme and press `Use This Theme`
+9. Congratulations. You just wasted 45-75 minutes looking at bad qt5 themes.
+</details>
+<details><summary> Kvantum is done. Now what???? </summary>
+# time for LXAppearance and QT5CT.
+
+1. Download said programs (if you use the KDE suite you might wanna get qt5ct-kde cuz better integration)
+2. Set themes in LXAppearance, Icon Theme, and Mouse Cursor Theme.
+3. Forget to hit apply and close LXAppearance
+4. Realize you wasted another 45 minutes choosing icon themes.
+5. Open Qt5ct
+6. Realize it's throwing errors at you
+7. Google frantically for another 25 minutes, searching for the answer on StackOverflow
+8. Export `QT_QPA_PLATFORMTHEME=qt5ct` in your environment variable
+9. Restart Qt5ct
+10. Select `kvantum` under `Style:`, feel free to mess around with the other settings too
+11. Hit apply.
+12. Changes should take effect immediately.
+
+
+# Walls
+Basically i stole some walls off the internet and stuck to one and some-what (terribly) made color palette, somewhat from pywall.
+
+Inspired by [@CordlessCoder](https://github.com/CordlessCoder)
+got me into wms in the first place (also stole a python script off him <3 thank you.)
---
+++ xmonad/xmonad.hs
@@ -0,0 +1,276 @@
+-- IMPORTS --
+
+-- main
+import XMonad
+import System.IO (hPutStrLn)
+import Data.Monoid
+import System.Exit
+import qualified XMonad.StackSet as W
+import qualified Data.Map        as M
+
+-- Actions
+import XMonad.Actions.MouseResize
+import XMonad.Actions.CycleWS
+import XMonad.Actions.Promote
+
+-- Hooks
+import XMonad.ManageHook
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.FadeInactive
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.WorkspaceHistory
+import XMonad.Hooks.ManageHelpers
+
+-- Utils
+import XMonad.Util.SpawnOnce
+import XMonad.Util.EZConfig
+import XMonad.Util.Run
+import XMonad.Util.NamedScratchpad as NS
+
+-- Layouts + mods
+import XMonad.Layout.ThreeColumns
+import XMonad.Layout.Spiral
+import XMonad.Layout.Tabbed
+import XMonad.Layout.Simplest
+import XMonad.Layout.NoBorders
+import XMonad.Layout.SubLayouts
+import XMonad.Layout.Renamed (renamed, Rename(Replace))
+import XMonad.Layout.LimitWindows (limitWindows, increaseLimit, decreaseLimit)
+import XMonad.Layout.MultiToggle.Instances
+import XMonad.Layout.MultiToggle
+import XMonad.Layout.ResizableTile
+import XMonad.Layout.LayoutModifier
+import XMonad.Layout.Spacing
+import XMonad.Layout.Gaps
+import XMonad.Layout.WindowArranger
+import XMonad.Layout.WindowNavigation
+import qualified XMonad.Layout.MultiToggle as MT (Toggle(..))
+
+-- self explanatory
+myTerminal      = "kitty"
+myFileManager   = "dolphin"
+myBrowser       = "firefox-developer-edition"
+myTedit         = "neovide"
+myLauncher      = "dmenu_run -l 15"
+
+-- focus follows mouse
+myFocusFollowsMouse :: Bool
+myFocusFollowsMouse = True
+
+myClickJustFocuses :: Bool
+myClickJustFocuses = False
+
+myBorderWidth   = 2
+
+myModMask       = mod4Mask -- windows key, change to mod1Mask for alt (i will hunt you)
+-- changing to alt also breaks some configs below
+
+-- b o r d e r s
+focusBordCol  = "#88d8e3"
+normBordCol = "#879BE3"
+
+-- startup hook (autostart type thing)
+myStartupHook :: X ()
+myStartupHook = do
+          spawnOnce "xrandr --output DisplayPort-0 --auto --output DisplayPort-1 --right-of DisplayPort-0"
+          spawnOnce "xsetroot -cursor_name left_ptr"
+          spawnOnce "picom"
+          spawnOnce "lxsession"
+          spawnOnce "wal -R"
+          spawnOnce "spotify"
+
+-- workspace names + window count + clickyyy
+xmobarEscape :: String -> String
+xmobarEscape = concatMap doubltLts
+ where
+        doubleLts '<' = "<<"
+        doubltLts x   = [x]
+
+myWorkspaces :: [String]
+myWorkspaces = clickable . map xmobarEscape
+               $ [" I "," II "," III "," IV "," V "," VI "," VII "," VIII "," IX "]
+
+  where
+        clickable l = [ "<action=xdotool key super+" ++ show n ++ ">" ++ ws ++ "</action>" |
+                      (i,ws) <- zip [1..9] l,
+                      let n = i ]
+
+windowCount :: X (Maybe String)
+windowCount = gets $ Just . show . length . W.integrate' . W.stack . W.workspace . W.current . windowset
+
+-- ScratchPads - basically floating temporary instances
+myScratchPads :: [NamedScratchpad]
+myScratchPads = [ NS "terminal" spawnTerm findTerm manageTerm
+                ]
+  where
+    spawnTerm  = myTerminal ++ " -T scratchpad"
+    findTerm   = title =? "scratchpad"
+    manageTerm = customFloating $ W.RationalRect l t w h
+               where
+                 h = 0.5
+                 w = 0.5
+                 t = 0.5 -h
+                 l = 0.5 -w
+
+-- spacing between window
+mySpacing :: Integer -> l a -> XMonad.Layout.LayoutModifier.ModifiedLayout Spacing l a
+mySpacing i = spacingRaw False (Border i i i i) True (Border i i i i) True
+
+
+
+myTabTheme = def { fontName            = "xft:FontAwesome-Regular"
+                 , activeColor         = "#88d8e3"
+                 , inactiveColor       = "#879BE3"
+                 , activeBorderColor   = "#88d8e3"
+                 , inactiveBorderColor = "#879BE3"
+                 , activeTextColor     = "#ffffff"
+                 , inactiveTextColor   = "#000000"
+                 }
+-- layouts:
+
+-- add more in format layout = renamed [Replace "layout"]
+-- customize options (found in docs) and add to myLayoutHook
+
+threeCol = renamed [Replace "threeCol"]
+           $ limitWindows 7
+           $ mySpacing 8
+           $ ThreeCol 1 (3/100) (1/2)
+tall     = renamed [Replace "tall"]
+           $ limitWindows 12
+           $ mySpacing 8
+           $ ResizableTall 1 (3/100) (1/2) []
+spirals  = renamed [Replace "spirals"]
+           $ smartBorders
+           $ windowNavigation
+           $ addTabs shrinkText myTabTheme
+           $ subLayout [] (smartBorders Simplest)
+           $ mySpacing 8
+           $ spiral (6/7)
+myLayoutHook = avoidStruts $ mouseResize $ windowArrange $  mkToggle (NBFULL ?? NOBORDERS ?? EOT) myLayout
+             where myLayout = spirals
+                         |||  threeCol
+                         |||  tall
+------------------------------------------------------------------------
+-- window rules:
+--
+-- find class name/other with xprop WM_CLASS
+--
+myManageHook :: XMonad.Query (Data.Monoid.Endo WindowSet)
+myManageHook = composeAll
+    [ className =? "mpv"            --> doShift (myWorkspaces !! 0)
+    , className =? "neovide"        --> doShift (myWorkspaces !! 0)
+    , className =? "Navigator"      --> doShift (myWorkspaces !! 1)
+    , className =? "firefoxdeveloperedition" --> doShift (myWorkspaces !! 1)
+    , className =? "discord"        --> doShift (myWorkspaces !! 2)
+    , className =? "Gimp"           --> doShift (myWorkspaces !! 3)
+    , className =? "spotify"        --> doShift (myWorkspaces !! 4)
+    , className =? "Lxappearance"   --> doCenterFloat
+    , className =? "qt5ct"          --> doCenterFloat
+    , className =? "dolphin"        --> doCenterFloat
+    , className =? "Font-manager"   --> doCenterFloat
+    , className =? "MPlayer"        --> doFloat
+    , className =? "Lutris"         --> doFloat
+    , resource  =? "desktop_window" --> doIgnore
+    ] <+> namedScratchpadManageHook myScratchPads
+
+------------------------------------------------------------------------
+-- event handling
+
+myEventHook = mempty
+
+------------------------------------------------------------------------
+-- status bars and logging
+
+myLogHook :: X ()
+myLogHook = fadeInactiveLogHook fadeAmount
+    where fadeAmount= 0.9
+
+------------------------------------------------------------------------
+-- Key bindings if it wasnt clear by the myKeys
+myKeys :: [(String, X ())]
+myKeys = [
+  -- XMonad stuff exit, recomile, restart
+  ("M-M1-q",     io (exitWith ExitSuccess))
+ ,("M-M1-r",     spawn "xmonad --recompile")
+ ,("M-S-r",      spawn "xmonad --restart")
+ ,("C-M-M1-l",   spawn "~/.xmonad/lock.sh")
+ ,("C-M-M1-q",   spawn "systemctl poweroff")
+
+  -- applications - terminal in a scratchpad, dmenu, text editor, browser, fileman
+ ,("M-<Return>", namedScratchpadAction myScratchPads "terminal")
+ ,("M-e",        spawn (myFileManager))
+ ,("M-s",        spawn "dmenu_run -l 15")
+ ,("M-c",        spawn (myTedit))
+ ,("M-S-c",      spawn (myBrowser))
+
+  -- g a p s x o x o
+ ,("C-M1-j",     decWindowSpacing 4)
+ ,("C-M1-k",     incWindowSpacing 4)
+ ,("C-M1-h",     decScreenSpacing 4)
+ ,("C-M1-l",     incScreenSpacing 4)
+
+  -- misc, namely screenshot + color picker + python script
+ ,("<Print>",    spawn "maim -s | xclip -selection clipboard -t image/png")
+ ,("M-n",        spawn "xcolor -f HEX! --scale 4 -s clipboard")
+ ,("M1-<Space>", spawn "python ~/.xmonad/change_layout.py")
+
+  -- kill windows
+ ,("C-S-q",      kill)
+ ,("M-b",        refresh)
+
+  -- focus windows n stuff
+ ,("M-<Tab>",   sendMessage NextLayout)
+ ,("M-f",       sendMessage (MT.Toggle NBFULL) >> sendMessage ToggleStruts)
+ ,("M-<Backspace>", promote)
+ ,("M-j",       windows W.focusDown)
+ ,("M-k",       windows W.focusUp)
+ ,("M-m",       windows W.focusMaster)
+
+ ,("M-S-j",     windows W.swapDown)
+ ,("M-S-k",     windows W.swapUp)
+
+ ,("M-M1-h",    sendMessage Shrink)
+ ,("M-M1-l",    sendMessage Expand)
+
+ ,("M-t",       withFocused $ windows . W.sink)
+
+ ,("M-.",       nextScreen)
+ ,("M-,",       prevScreen)
+
+ ,("M-S-<Up>",   sendMessage (IncMasterN 1))
+ ,("M-S-<Down>", sendMessage (IncMasterN (-1)))
+
+ ]
+
+------------------------------------------------------------------------
+-- main function
+main :: IO ()
+main = do
+    xmproc0 <- spawnPipe "xmobar -x 1 ~/.config/xmobar/xmobarrc"
+    xmproc1 <- spawnPipe "xmobar -x 2 ~/.config/xmobar/xmobarrc1"
+    xmonad $ docks def {
+        terminal           = myTerminal,
+        focusFollowsMouse  = myFocusFollowsMouse,
+        clickJustFocuses   = myClickJustFocuses,
+        borderWidth        = myBorderWidth,
+        modMask            = myModMask,
+        workspaces         = myWorkspaces,
+        normalBorderColor  = normBordCol,
+        focusedBorderColor = focusBordCol,
+        layoutHook         = myLayoutHook,
+        manageHook         = myManageHook <+> manageDocks,
+        handleEventHook    = myEventHook,
+        startupHook        = myStartupHook,
+        logHook            = workspaceHistoryHook <+> myLogHook <+> dynamicLogWithPP xmobarPP
+        { ppOutput = \x -> hPutStrLn xmproc0 x >> hPutStrLn xmproc1 x
+        , ppCurrent = xmobarColor "#f7768e" "" . wrap ("<box type=Bottom width=2 mb=2 color=#f7768e>") "</box>"
+        , ppVisible = xmobarColor "#f7768e" ""
+        , ppHidden = xmobarColor "#c0caf5" "" . wrap ("<box type=Top width=2 mt=2 color=#c0caf5>") "</box>"
+        , ppHiddenNoWindows = xmobarColor "#c0caf5" ""
+        , ppTitle = xmobarColor "#f5f5f5" "" . shorten 60
+        , ppSep = "<fc=#12162a> <fn=1></fn> </fc>"
+        , ppUrgent = xmobarColor "#ea1a25" "" .wrap "!" "!"
+        , ppExtras = [windowCount]
+        , ppOrder = \(ws:l:t:ex) -> [ws,l]++ex++[t]
+        }
+           } `additionalKeysP` myKeys
