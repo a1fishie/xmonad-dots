0f12703a3ddc6b84a7a8056cbe12ef292400dace
a1fishie <104651507+a1fishie@users.noreply.github.com>
2022-07-06 22:47:31 +0100

    broke scratchpads

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,7 +1,8 @@
 -- IMPORTS --
 -- main
 import XMonad
-import System.IO (hPutStrLn)
+import System.IO
+import Control.Monad
 import Data.Monoid
 import System.Exit
 import XMonad.Operations
@@ -10,38 +11,33 @@ import qualified Data.Map        as M
 
 -- Actions
 import XMonad.Actions.MouseResize
-import XMonad.Actions.CycleWS
+import XMonad.Actions.CycleWS as C
 import XMonad.Actions.Promote
 
 -- Hooks
 import XMonad.ManageHook
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.FadeInactive
-import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.DynamicProperty
+import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.WorkspaceHistory
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.InsertPosition
+import XMonad.Hooks.SetWMName
 
 -- Utils
 import XMonad.Util.SpawnOnce
 import XMonad.Util.EZConfig
 import XMonad.Util.Run
-import XMonad.Util.NamedScratchpad as NS
+import XMonad.Util.NamedScratchpad
 
 -- Layouts + mods
-import XMonad.Layout.ThreeColumns
-import XMonad.Layout.GridVariants (Grid(Grid))
-import XMonad.Layout.Spiral
-import XMonad.Layout.Tabbed
-import XMonad.Layout.Simplest
 import XMonad.Layout.NoBorders
-import XMonad.Layout.SubLayouts
-import XMonad.Layout.Renamed (renamed, Rename(Replace))
-import XMonad.Layout.LimitWindows (limitWindows, increaseLimit, decreaseLimit)
+import XMonad.Layout.Renamed
+import XMonad.Layout.LimitWindows
+import XMonad.Layout.NoBorders
 import XMonad.Layout.MultiToggle.Instances
 import XMonad.Layout.MultiToggle
-import XMonad.Layout.ResizableTile
 import XMonad.Layout.LayoutModifier
 import XMonad.Layout.Spacing
 import XMonad.Layout.Gaps
@@ -55,121 +51,167 @@ myFileManager   = "dolphin"
 myBrowser       = "firefox-developer-edition"
 myTedit         = "neovide"
 myLauncher      = "dmenu_run"
+myModMask       = mod4Mask
 
--- focus follows mouse
-myFocusFollowsMouse :: Bool
+-- does window focus follows mouse
 myFocusFollowsMouse = False
-myClickJustFocuses :: Bool
 myClickJustFocuses = False
 
-myBorderWidth   = 2
-
-myModMask       = mod4Mask -- windows key, change to mod1Mask for alt (i will hunt you)
--- changing to alt also breaks some configs below
-
 -- b o r d e r s
+myBorderWidth = 3
 focusBordCol  = "#89dced"
-normBordCol = "#898b4fa"
+normBordCol   = "#898b4fa"
+
+------------------------------------------------------------------------
+-- Key bindings if it wasnt clear by the myKeys
+-- changing mod key to alt also breaks some configs below
+myKeys =
+  powerKeys
+    ++ launchKeys
+    ++ miscKeys
+    ++ layoutKeys
+    ++ wmKeys
+  where
+    powerKeys =
+      [  ("M-M1-q",     io (exitWith ExitSuccess))
+        ,("M-M1-r",     spawn "xmonad --recompile")
+        ,("M-S-r",      spawn "xmonad --restart")
+        ,("C-M-M1-l",   spawn "~/.xmonad/lock.sh")
+        ,("C-M-M1-q",   spawn "systemctl poweroff")
+        ,("C-M-M1-r",   spawn "systemctl reboot")
+      ]
+
+    launchKeys =
+      [  ("M-S-<Return>", spawn (myTerminal))
+        ,("M-<Return>", openScratchPad "terminal")
+        ,("M-m",        openScratchPad "mixer")
+        ,("M-e",        spawn (myFileManager))
+        ,("M-s",        spawn "dmenu_run -l 15")
+        ,("M-c",        spawn (myTedit))
+        ,("M-S-c",      spawn (myBrowser))
+        ,("M-M1-c",     spawn "emacsclient -c -a 'emacs'")
+      ]
+
+    miscKeys =
+      [  ("<Print>",    spawn "maim -s | xclip -selection clipboard -t image/png")
+        ,("M-n",        spawn "xcolor -f HEX! --scale 4 -s clipboard")
+        ,("M1-<Space>", spawn "python ~/.xmonad/change_layout.py")
+        ,("C-S-q",      kill)
+      ]
+
+    layoutKeys =
+      [  ("M-<Tab>",   sendMessage NextLayout)
+        ,("M-f",       sendMessage (MT.Toggle NBFULL))
+        ,("M-C-f",     sendMessage (MT.Toggle NBFULL) >> sendMessage ToggleStruts)
+        ,("M-<Backspace>", promote)
+        ,("M-j",       windows W.focusDown)
+        ,("M-h",       windows W.focusDown)
+        ,("M-l",       windows W.focusUp)
+        ,("M-k",       windows W.focusUp)
+        ,("M-t",       withFocused $ toggleFloat $ rectCentered 0.7)
+        ,("M-S-t",     withFocused $ toggleFloat $ vertRectCentered 0.9)
+        ,("M-[",       sendMessage (IncMasterN 1)),
+         ("M-]",       sendMessage (IncMasterN (-1)))
+      ]
+
+    wmKeys =
+      [  ("M-S-j",     windows W.swapDown)
+        ,("M-S-k",     windows W.swapUp)
+        ,("M-,",       nextScreen)
+        ,("M-M1-h",    sendMessage Shrink)
+        ,("M-M1-l",    sendMessage Expand)
+      ]
+
+removedKeys :: [String]
+removedKeys = ["M-h", "M-l", "M-."]
+
+myKeysConfig :: XConfig a -> XConfig a
+myKeysConfig config = config `additionalKeysP` myKeys `removeKeysP` removedKeys
+
+------------------------------------------------------------------------
 
 -- startup hook (autostart type thing)
 myStartupHook :: X ()
 myStartupHook = do
-          spawnOnce "xrandr --output DisplayPort-0 --auto --output DisplayPort-1 --right-of DisplayPort-0"
-          spawnOnce "xsetroot -cursor_name left_ptr"
-          spawnOnce "picom"
-          spawnOnce "lxsession"
-          spawnOnce "wal -R"
-          spawnOnce "spotify"
+        traverse spawnOnce
+          [ "xrandr --output DisplayPort-0 --auto --output DisplayPort-1 --right-of DisplayPort-0"
+          , "xsetroot -cursor_name left_ptr"
+	  , "/usr/bin/emacs --daemon"
+          , "picom"
+          , "lxsession"
+          , "wal -R"
+          , "spotify"
+          ]
+	setWMName "LG3D"
 
 -- workspace names + window count + clickyyy
-xmobarEscape :: String -> String
-xmobarEscape = concatMap doubltLts
- where
-        doubleLts '<' = "<<"
-        doubltLts x   = [x]
 
-myWorkspaces :: [String]
-myWorkspaces = clickable . map xmobarEscape
-               $ ["I","II","III","IV","V","VI","VII","VIII","IX"]
-
-  where
-        clickable l = [ "<action=xdotool key super+" ++ show n ++ ">" ++ ws ++ "</action>" |
-                      (i,ws) <- zip [1..9] l,
-                      let n = i ]
+myWorkspaces = ["I","II","III","IV","V","VI","VII", "VIII", "IX"]
+ignoredWorkspaces = ["NSP"]
 
 windowCount :: X (Maybe String)
 windowCount = gets $ Just . show . length . W.integrate' . W.stack . W.workspace . W.current . windowset
 
 -- ScratchPads - basically floating temporary instances
 myScratchPads :: [NamedScratchpad]
-myScratchPads = [ NS "terminal" spawnTerm findTerm manageTerm
-                ]
+myScratchPads = [mixer, terminal]
   where
-    spawnTerm  = myTerminal ++ " -T scratchpad"
-    findTerm   = title =? "scratchpad"
-    manageTerm = customFloating $ W.RationalRect l t w h
-               where
-                 h = 0.5
-                 w = 0.5
-                 t = 0.25
-                 l = 0.25
+    terminal = NS "terminal" spawn find manage
+      where
+        spawn = myTerminal ++ " -t scratchpad"
+        find = title =? "terminal"
+        manage = customFloating $ rectCentered 0.4
+    mixer = NS "mixer" spawn find manage
+      where
+        spawn = myTerminal ++ " -t pulsepixer -e pulsemixer"
+        find = title =? "pulsemixer"
+        manage = customFloating $ rectCentered 0.6
 
--- spacing between window
+openScratchPad :: String -> X ()
+openScratchPad = namedScratchpadAction myScratchPads
+
+rectCentered :: Rational -> W.RationalRect
+rectCentered percentage = W.RationalRect offset offset percentage percentage
+  where
+    offset = (1 - percentage) / 2
+
+
+vertRectCentered :: Rational -> W.RationalRect
+vertRectCentered height = W.RationalRect offsetX offsetY width height
+  where
+    width = height / 2
+    offsetX = (1 - width) / 2
+    offsetY = (1 - height) / 2
+
+-- Layouts
+
+defaultTall = Tall 1 0.05 0.6
+
+tall = renamed [Replace "Default"] $ limitWindows 6 $ defaultSpacing defaultTall
+
+myLayout = avoidStruts $ mkToggle (NBFULL ?? NOBORDERS ?? EOT) tall
+
 mySpacing :: Integer -> l a -> XMonad.Layout.LayoutModifier.ModifiedLayout Spacing l a
 mySpacing i = spacingRaw False (Border i i i i) True (Border i i i i) True
 
+defaultSpacing :: l a -> XMonad.Layout.LayoutModifier.ModifiedLayout Spacing l a
+defaultSpacing = mySpacing 4
 
-myTabTheme = def { fontName            = "xft:Meslo LG S"
-                 , activeColor         = "#89dceb"
-                 , inactiveColor       = "#f9e2af"
-                 , activeBorderColor   = "#89dceb"
-                 , inactiveBorderColor = "#f9e2af"
-                 , activeTextColor     = "#ffffff"
-                 , inactiveTextColor   = "#000000"
-                 }
--- layouts:
-
--- add more in format layout = renamed [Replace "layout"]
--- customize options (found in docs) and add to myLayoutHook
-
-threeCol = renamed [Replace "threeCol"]
-           $ windowNavigation
-           $ limitWindows 7
-           $ mySpacing 8
-           $ ThreeCol 1 (3/100) (1/2)
-tall     = renamed [Replace "tall"]
-           $ limitWindows 12
-           $ mySpacing 8
-           $ ResizableTall 1 (3/100) (1/2) []
-spirals  = renamed [Replace "spirals"]
-           $ smartBorders
-           $ windowNavigation
-           $ addTabs shrinkText myTabTheme
-           $ subLayout [] (smartBorders Simplest)
-           $ mySpacing 8
-           $ spiral (6/7)
-grid     = renamed [Replace "grid"]
-           $ smartBorders
-           $ windowNavigation
-           $ addTabs shrinkText myTabTheme
-           $ subLayout [] (smartBorders Simplest)
-           $ limitWindows 12
-           $ mySpacing 8
-           $ mkToggle (single MIRROR)
-           $ Grid (16/10)
-myLayoutHook = avoidStruts $ mouseResize $ windowArrange $  mkToggle (NBFULL ?? NOBORDERS ?? EOT) myLayout
-             where myLayout = threeCol
-                         |||  grid
-                         |||  spirals
-                         |||  tall
+toggleFloat :: W.RationalRect -> Window -> X ()
+toggleFloat r w =
+  windows
+    ( \s ->
+        if M.member w (W.floating s)
+          then W.sink w s
+          else W.float w r s
+    )
 ------------------------------------------------------------------------
 -- window rules:
---
 -- find class name/other with xprop WM_CLASS
---
 myManageHook :: XMonad.Query (Data.Monoid.Endo WindowSet)
 myManageHook = composeAll
     [ className =? "mpv"             --> doShift (myWorkspaces !! 0)
+    , className =? "dotnet"          --> doShift (myWorkspaces !! 0)
     , className =? "Navigator"       --> doShift (myWorkspaces !! 1)
     , className =? "firefoxdeveloperedition" --> doShift (myWorkspaces !! 1)
     , className =? "discord"         --> doShift (myWorkspaces !! 2)
@@ -183,10 +225,14 @@ myManageHook = composeAll
     , className =? "Kvantum Manager" --> doCenterFloat
     , className =? "dolphin"         --> doCenterFloat
     , className =? "Font-manager"    --> doCenterFloat
+    , className =? "Xmessage"        --> doCenterFloat
     , className =? "MPlayer"         --> doFloat
     , className =? "Lutris"          --> doFloat
     , resource  =? "desktop_window"  --> doIgnore
-    ] <+> namedScratchpadManageHook myScratchPads
+    ,insertPosition End Older
+    , manageDocks
+    , namedScratchpadManageHook myScratchPads
+    ]
 
 ------------------------------------------------------------------------
 -- event handling
@@ -197,87 +243,27 @@ myEventHook = dynamicPropertyChange "WM_NAME" (title =? "Spotify" --> doShift (m
 
 myLogHook :: X ()
 myLogHook = fadeInactiveLogHook fadeAmount
-    where fadeAmount= 0.9
+    where fadeAmount = 0.85
 
-------------------------------------------------------------------------
--- Key bindings if it wasnt clear by the myKeys
-myKeys :: [(String, X ())]
-myKeys = [
-  -- XMonad stuff exit, recomile, restart
-  ("M-M1-q",     io (exitWith ExitSuccess))
- ,("M-M1-r",     spawn "xmonad --recompile")
- ,("M-S-r",      spawn "xmonad --restart")
- ,("C-M-M1-l",   spawn "~/.xmonad/lock.sh")
- ,("C-M-M1-q",   spawn "systemctl poweroff")
- ,("C-M-M1-r",   spawn "systemctl reboot")
-
- -- applications - terminal in a scratchpad, dmenu, text editor, browser, fileman
- ,("M-<Return>", namedScratchpadAction myScratchPads "terminal")
- ,("M-e",        spawn (myFileManager))
- ,("M-s",        spawn "dmenu_run -l 15")
- ,("M-c",        spawn (myTedit))
- ,("M-S-c",      spawn (myBrowser))
-
-  -- g a p s x o x o
- ,("C-M1-j",     decWindowSpacing 4)
- ,("C-M1-k",     incWindowSpacing 4)
- ,("C-M1-h",     decScreenSpacing 4)
- ,("C-M1-l",     incScreenSpacing 4)
-
-  -- misc, namely screenshot + color picker + python script
- ,("<Print>",    spawn "maim -s | xclip -selection clipboard -t image/png")
- ,("M-n",        spawn "xcolor -f HEX! --scale 4 -s clipboard")
- ,("M1-<Space>", spawn "python ~/.xmonad/change_layout.py")
-
-  -- kill windows
- ,("C-S-q",      kill)
- ,("M-b",        refresh)
-
-  -- focus windows n stuff
- ,("M-<Tab>",   sendMessage NextLayout)
- ,("M-f",       sendMessage (MT.Toggle NBFULL))
- ,("M-C-f",     sendMessage (MT.Toggle NBFULL) >> sendMessage ToggleStruts)
- ,("M-<Backspace>", promote)
- ,("M-j",       windows W.focusDown)
- ,("M-k",       windows W.focusUp)
- ,("M-m",       windows W.focusMaster)
-
- ,("M-S-j",     windows W.swapDown)
- ,("M-S-k",     windows W.swapUp)
-
- ,("M-M1-h",    sendMessage Shrink)
- ,("M-M1-l",    sendMessage Expand)
-
- ,("M-t",       withFocused $ windows . W.sink)
-
- ,("M-.",       nextScreen)
- ,("M-,",       prevScreen)
-
- ,("M-S-<Up>",   sendMessage (IncMasterN 1))
- ,("M-S-<Down>", sendMessage (IncMasterN (-1)))
-
- ]
-
-------------------------------------------------------------------------
 -- main function
 main :: IO ()
 main = do
     xmproc0 <- spawnPipe "xmobar -x 1 ~/.config/xmobar/xmobarrc"
-    xmproc1 <- spawnPipe "xmobar -x 2 ~/.config/xmobar/xmobarrc1"
+    xmproc1 <- spawnPipe "xmobar -x 2 ~/.config/xmobar/xmobarrc"
     xmonad $ docks def {
         terminal           = myTerminal,
+        modMask            = myModMask,
         focusFollowsMouse  = myFocusFollowsMouse,
         clickJustFocuses   = myClickJustFocuses,
         borderWidth        = myBorderWidth,
-        modMask            = myModMask,
         workspaces         = myWorkspaces,
         normalBorderColor  = normBordCol,
         focusedBorderColor = focusBordCol,
-        layoutHook         = myLayoutHook,
-        manageHook         = insertPosition End Older <+> myManageHook <+> manageDocks,
+        layoutHook         = myLayout,
+        manageHook         = myManageHook,
         handleEventHook    = myEventHook ,
         startupHook        = myStartupHook,
-        logHook            = workspaceHistoryHook <+> myLogHook <+> dynamicLogWithPP xmobarPP
+        logHook            = myLogHook <+> dynamicLogWithPP  xmobarPP
         { ppOutput = \x -> hPutStrLn xmproc0 x >> hPutStrLn xmproc1 x
         , ppCurrent = xmobarColor "#f9e4a3" "" . wrap ("<box type=Bottom width=2 mb=2 color=#f9e4a3>") "</box>"
         , ppVisible = xmobarColor "#f9e4a3" ""
@@ -287,6 +273,7 @@ main = do
         , ppSep = "<fc=#12162a> <fn=1></fn> </fc>"
         , ppUrgent = xmobarColor "#d20f39" "" .wrap "!" "!"
         , ppExtras = [windowCount]
-        , ppOrder = \(ws:l:t:ex) -> [ws,l]++ex++[t]
+        , ppOrder = \(ws:l:t:ex) -> [ws]++ex++[t]
         }
            } `additionalKeysP` myKeys
+------------------------------------------------------------------------
